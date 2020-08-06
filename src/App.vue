ce82c929585df9315199c59ea6d03ed50880adaa
Dennis O'Connell <doconnell@scu.edu>
2020-08-06 14:07:50 -0700

    having error

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <template>
   <div id="app">
-    
+    <Todos />
   </div>
 </template>
 
 <script>
+import Todos from '@components/Todos.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Todos
   },
   data(){
     return{
       todos: [
         {
-          
+          id:1,
+          title:"Todo One",
+          completed:false
+        },
+        {
+          id:2,
+          title:"Todo Two",
+          completed:true
         }
       ]
     }
