a8cf352a6c9dc6226c77e41464fdd21405f0cdd2
Dennis O'Connell <doconnell@scu.edu>
2020-08-09 22:11:22 -0700

    completed Todos.vue so that it can loop through todos data with v-for directive from vue

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,13 +1,16 @@
 <template>
     <div>
-        <h1> Todos </h1>
+        <div v-for="todo in todos" :key="todo.id">
+            <h3>{{todo.title}}</h3>
+        </div>
     </div>
      
 </template>
 
 <script>
 export default {
-    name: "Todos"
+    name: "Todos",
+    props: ["todos"]
 }
 </script>
 
