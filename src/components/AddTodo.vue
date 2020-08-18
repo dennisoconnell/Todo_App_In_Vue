ba0505e06e8c331197e30502b9f4a3d671edf249
Dennis O'Connell <doconnell@scu.edu>
2020-08-18 11:57:58 -0700

    began addTodo method

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <form>
-            <input type="text" name="title" placeholder="Add Todo...">
+        <form @submit="addTodo">
+            <input type="text" name="title" v-model="title" placeholder="Add Todo...">
 
             <input type="submit" value="Submit" class="btn">
 
@@ -11,8 +11,18 @@
 
 <script>
 export default {
-    name: "AddTodo"
-}
+    name: "AddTodo",
+    data(){
+      return{
+        title:''
+      }
+    },
+    methods: {
+      addTodo() {
+        
+      }  
+    }
+  }
 </script>
 
 <style scoped>
